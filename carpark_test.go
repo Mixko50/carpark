88255a99e154ef45805b9cc3a1973269678ecf59
Mixko <apisitmaneerat@gmail.com>
2024-07-07 16:37:19 +0700

    fix: more than 24 hours logic

--- carpark_test.go
+++ carpark_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestParkLessThanTwoHours(t *testing.T) {
 	type testCase struct {
+		name      string
 		parkTime  time.Time
 		leaveTime time.Time
 		expected  int
@@ -15,23 +16,27 @@ func TestParkLessThanTwoHours(t *testing.T) {
 	tests := []testCase{
 		{
 			// 1 hour
+			"1 hour",
 			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 1, 11, 0, 0, 0, time.UTC),
 			0,
 		},
 		{
 			// 1 hour 59 minutes
+			"1 hour 59 minutes",
 			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 1, 11, 59, 0, 0, time.UTC),
 			0,
 		},
 		{
 			// 2 hours
+			"2 hours",
 			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 1, 12, 0, 0, 0, time.UTC),
 			0,
 		},
 		{
+			"1 hour 3 minutes 2 seconds",
 			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 1, 1, 3, 2, 0, time.UTC),
 			0,
@@ -39,50 +44,18 @@ func TestParkLessThanTwoHours(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
-		if result != test.expected {
-			t.Errorf("Expected %d, got %d", test.expected, result)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
+			if result != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
 	}
 }
 
-func TestParkMoreThanTwoHours(t *testing.T) {
+func TestParkMoreThanTwoHoursAndBeforeTenPm(t *testing.T) {
 	type testCase struct {
-		parkTime  time.Time
-		leaveTime time.Time
-		expected  int
-	}
-	tests := []testCase{
-		{
-			// 3 hours
-			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 1, 13, 0, 0, 0, time.UTC),
-			100,
-		},
-		{
-			// 6 hours
-			time.Date(2019, time.January, 1, 12, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 1, 18, 0, 0, 0, time.UTC),
-			400,
-		},
-		{
-			// 4 hours 1 minute
-			time.Date(2019, time.January, 1, 11, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 1, 15, 0, 1, 0, time.UTC),
-			300,
-		},
-	}
-
-	for _, test := range tests {
-		result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
-		if result != test.expected {
-			t.Errorf("Expected %d, got %d", test.expected, result)
-		}
-	}
-}
-
-func TestParkMoreThanTwoHoursAndLeaveAfterTenPmButBeforeTenPm(t *testing.T) {
-	type testCase struct {
+		name      string
 		parkTime  time.Time
 		leaveTime time.Time
 		expected  int
@@ -90,64 +63,151 @@ func TestParkMoreThanTwoHoursAndLeaveAfterTenPmButBeforeTenPm(t *testing.T) {
 	tests := []testCase{
 		{
 			// 2 hours
+			"2 hours",
 			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 1, 22, 0, 0, 0, time.UTC),
 			0,
 		},
 		{
-			// 2 hours 1 minute
-			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 1, 22, 1, 0, 0, time.UTC),
-			1000,
-		},
-		{
 			// 3 hours
-			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 1, 23, 0, 0, 0, time.UTC),
-			1000,
-		},
-		{
-			// 6 hours, day 1 from 20:00 to 23:59, day 2 from 00:00 to 02:00
-			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 2, 2, 0, 0, 0, time.UTC),
-			1000,
-		},
-		{
-			// 8 hours, day 1 from 19:00 to 23:59, day 2 from 00:00 to 03:00
-			time.Date(2019, time.January, 1, 19, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 2, 3, 0, 0, 0, time.UTC),
-			1100,
-		},
-		{
-			// 19 hours, day 1 from 19:00 to 23:59, day 2 from 00:00 to 10:00
-			time.Date(2019, time.January, 1, 13, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 2, 8, 0, 0, 0, time.UTC),
-			1700,
-		},
-		{
-			// 24 hours, day 1 from 10:00 to 23:59, day 2 from 00:00 to 10:00
+			"3 hours",
 			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
-			time.Date(2019, time.January, 2, 10, 0, 0, 0, time.UTC),
-			2000,
+			time.Date(2019, time.January, 1, 13, 0, 0, 0, time.UTC),
+			100,
+		},
+		{
+			// 6 hours
+			"6 hours",
+			time.Date(2019, time.January, 1, 12, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 1, 18, 0, 0, 0, time.UTC),
+			400,
+		},
+		{
+			// 4 hours 1 minute
+			"4 hours 1 minute",
+			time.Date(2019, time.January, 1, 11, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 1, 15, 0, 1, 0, time.UTC),
+			300,
+		},
+		{
+			// 5 hours 25 minutes 52 seconds
+			"5 hours 25 minutes 52 seconds",
+			time.Date(2019, time.January, 1, 12, 10, 4, 0, time.UTC),
+			time.Date(2019, time.January, 1, 17, 35, 56, 0, time.UTC),
+			400,
+		},
+		{
+			// 20 hours 23 minutes 2 seconds
+			"20 hours 23 minutes 2 seconds",
+			time.Date(2019, time.January, 1, 11, 5, 4, 0, time.UTC),
+			time.Date(2019, time.January, 1, 21, 38, 6, 0, time.UTC),
+			900,
 		},
 	}
 
 	for _, test := range tests {
-		result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
-		if result != test.expected {
-			t.Errorf("Expected %d, got %d", test.expected, result)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
+			if result != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
 	}
 }
 
-func TestParkMoreThan24Hours(t *testing.T) {
+func TestParkMoreThanTwoHoursAndLeaveAfterTenPmButBeforeTenPm(t *testing.T) {
 	type testCase struct {
+		name      string
 		parkTime  time.Time
 		leaveTime time.Time
 		expected  int
 	}
 	tests := []testCase{
 		{
+			// 2 hours 1 minute
+			"2 hours 1 minute",
+			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 1, 22, 1, 0, 0, time.UTC),
+			1000,
+		},
+		{
+			// 3 hours
+			"3 hours",
+			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 1, 23, 0, 0, 0, time.UTC),
+			1000,
+		},
+		{
+			// 6 hours, day 1 from 20:00 to 23:59, day 2 from 00:00 to 02:00
+			"6 hours",
+			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 2, 2, 0, 0, 0, time.UTC),
+			1000,
+		},
+		{
+			// 8 hours, day 1 from 19:00 to 23:59, day 2 from 00:00 to 03:00
+			"8 hours",
+			time.Date(2019, time.January, 1, 19, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 2, 3, 0, 0, 0, time.UTC),
+			1100,
+		},
+		{
+			// 19 hours, day 1 from 19:00 to 23:59, day 2 from 00:00 to 10:00
+			"19 hours",
+			time.Date(2019, time.January, 1, 13, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 2, 8, 0, 0, 0, time.UTC),
+			1700,
+		},
+		{
+			// 23 hours, day 1 from 10:00 to 23:59, day 2 from 00:00 to 9:00
+			"23 hours",
+			time.Date(2019, time.January, 1, 10, 0, 0, 0, time.UTC),
+			time.Date(2019, time.January, 2, 9, 0, 0, 0, time.UTC),
+			2000,
+		},
+		{
+			"23 hours, 32 minutes, 12 seconds",
+			time.Date(2019, time.January, 1, 15, 7, 28, 0, time.UTC),
+			time.Date(2019, time.January, 2, 14, 39, 40, 0, time.UTC),
+			2000,
+
+			// day 1: 15:07 - 17:07 -> Free
+			// day 1: 17:08 - 21:59 -> 500
+			// day 1: 22:00 - 09:59 -> 1000
+			// day 2: 10:00 - 14:39 -> 500
+			// 500 + 1000 + 500 = 2000
+		},
+		{
+			"7 hours, 30 minutes, 12 seconds",
+			time.Date(2019, time.January, 1, 21, 30, 28, 0, time.UTC),
+			time.Date(2019, time.January, 2, 5, 0, 40, 0, time.UTC),
+			1000,
+
+			// day 1: 21:30 - 22:00 -> Free
+			// day 1: 22:00 - 05:00 -> 1000
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
+			if result != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestParkMoreThan24Hours(t *testing.T) {
+	type testCase struct {
+		name      string
+		parkTime  time.Time
+		leaveTime time.Time
+		expected  int
+	}
+	tests := []testCase{
+		{
+			"25 hours",
 			time.Date(2019, time.January, 3, 21, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 4, 22, 0, 0, 0, time.UTC),
 			3200,
@@ -159,6 +219,7 @@ func TestParkMoreThan24Hours(t *testing.T) {
 			// 1000 + 1200 + 1000 = 3200
 		},
 		{
+			"49 hours",
 			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 3, 21, 0, 0, 0, time.UTC),
 			4300,
@@ -170,11 +231,21 @@ func TestParkMoreThan24Hours(t *testing.T) {
 			// day 3: 10 - 21 -> 1100
 		},
 		{
+			"50 hours",
 			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 3, 22, 0, 0, 0, time.UTC),
 			5400,
+
+			// day 1: 20 - 22 -> Free
+			// day 2: 22 - 9:59 -> 1000
+			// day 2: 10 - 21:59 -> 1200
+			// day 3: 22 - 9:59 -> 1000
+			// day 3: 10 - 21:59 -> 1200
+			// day 3: 22 -> 1000
+			// 1000 + 1200 + 1000 + 1200 + 1000 = 5400
 		},
 		{
+			"98 hours",
 			time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC),
 			time.Date(2019, time.January, 5, 23, 0, 0, 0, time.UTC),
 			9800,
@@ -191,12 +262,20 @@ func TestParkMoreThan24Hours(t *testing.T) {
 			// day 5: 22 - 23 -> 1000
 			// 1000 + 1200 + 1000 + 1200 + 1000 + 1200 + 1000 + 1200 + 1000 = 9800
 		},
+		{
+			"18 days, 12 hours, 19 minutes, 8 seconds",
+			time.Date(2019, time.January, 1, 13, 45, 34, 0, time.UTC),
+			time.Date(2019, time.January, 20, 2, 4, 42, 0, time.UTC),
+			41300,
+		},
 	}
 
 	for _, test := range tests {
-		result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
-		if result != test.expected {
-			t.Errorf("Expected %d, got %d", test.expected, result)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			result, _ := CalculateParkingFee(test.parkTime, test.leaveTime)
+			if result != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
 	}
 }
--- main.go
+++ main.go
@@ -11,25 +11,30 @@ import (
 // เมื่อ 22:00 ถึง 10:00 จะเสียค่าปรับ 1000 บาท
 // เศษของชั่วโมงถือเป็นชั่วโมงเต็ม
 
+func main() {
+	fmt.Println("Hello, playground")
+	//parkTime := time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC)
+	//leaveTime := time.Date(2019, time.January, 3, 21, 0, 0, 0, time.UTC)
+	//parkTime := time.Date(2019, time.January, 3, 21, 0, 0, 0, time.UTC)
+	//leaveTime := time.Date(2019, time.January, 4, 22, 0, 0, 0, time.UTC)
+	parkTime := time.Date(2019, time.January, 1, 15, 7, 28, 0, time.UTC)
+	leaveTime := time.Date(2019, time.January, 2, 14, 39, 40, 0, time.UTC)
+	//leaveTime := time.Date(2019, time.January, 4, 22, 0, 0, 0, time.UTC)
+	fmt.Println(CalculateParkingFee(parkTime, leaveTime))
+}
+
 const (
 	pricePerHour        = 100
 	freeParkingHours    = 2
 	suspensionStartTime = 22
 	suspensionEndTime   = 10
 	suspensionFee       = 1000
+	dailyParkingFee     = 2200
 )
-
-func main() {
-	fmt.Println("Hello, playground")
-	//parkTime := time.Date(2019, time.January, 1, 20, 0, 0, 0, time.UTC)
-	//leaveTime := time.Date(2019, time.January, 3, 21, 0, 0, 0, time.UTC)
-	parkTime := time.Date(2019, time.January, 3, 21, 0, 0, 0, time.UTC)
-	leaveTime := time.Date(2019, time.January, 4, 22, 0, 0, 0, time.UTC)
-	fmt.Println(CalculateParkingFee(parkTime, leaveTime))
-}
 
 func CalculateParkingFee(parkTime, leaveTime time.Time) (int, error) {
 	// Round up the seconds to be a minute
+	startSuspensionTime := time.Date(parkTime.Year(), parkTime.Month(), parkTime.Day(), suspensionStartTime, 0, 0, 0, time.UTC)
 	if leaveTime.Second() != 0 {
 		leaveTime = leaveTime.Add(time.Minute).Truncate(time.Minute)
 	}
@@ -49,39 +54,48 @@ func CalculateParkingFee(parkTime, leaveTime time.Time) (int, error) {
 		dayDifference := int(leaveTime.Sub(parkTime).Hours() / 24)
 
 		// Calculate price for each day
-		// 2200 is the price for 24 hours
-		// 200 is the discount for first 2 hours
-		totalPrice += 2200 * dayDifference
+		// 2200 is the price for 24 hours without free parking time
+		totalPrice += dailyParkingFee * dayDifference
+
+		// If the park time is not 21:00, then we need to deduct 200 baht for first 2 hours of parking fee
 		if dayDifference > 1 && parkTime.Hour() != 21 {
 			totalPrice -= 200
 		}
 
+		// If the leave time is 21:00, then we need to deduct 100 baht for only first an hour of parking fee
 		if parkTime.Hour() == 21 {
 			totalPrice -= 100
 		}
 
 		// Set the park time to be the day as leave time
-		parkTime = parkTime.Add(time.Hour * 24 * time.Duration(dayDifference))
+		tempParkTime := parkTime.Add(time.Hour * 24 * time.Duration(dayDifference))
 
 		// Calculate the actual minutes for the last day
-		actualMinutes = math.Abs(leaveTime.Sub(parkTime).Minutes())
+		actualMinutes = math.Abs(leaveTime.Sub(tempParkTime).Minutes())
 
 		if isSuspensionTime(leaveTime) {
 			totalPrice += suspensionFee
 
 			// Cut the suspension time
-			actualMinutes -= leaveTime.Sub(time.Date(leaveTime.Year(), leaveTime.Month(), leaveTime.Day(), 22, 0, 0, 0, time.UTC)).Minutes()
+			actualMinutes -= leaveTime.Sub(time.Date(leaveTime.Year(), leaveTime.Month(), leaveTime.Day(), suspensionStartTime, 0, 0, 0, time.UTC)).Minutes()
 		}
-
-		totalPrice += CalculateParkingPriceWithTime(actualMinutes, pricePerHour)
+		if startSuspensionTime.Sub(parkTime).Minutes() > freeParkingTime() {
+			tempActualMinutes := startSuspensionTime.Sub(parkTime).Minutes()
+			totalPrice += CalculateParkingPriceWithTime(tempActualMinutes, pricePerHour)
+		} else {
+			totalPrice += CalculateParkingPriceWithTime(actualMinutes, pricePerHour)
+		}
 		return totalPrice, nil
 	}
 
 	// Only one day
-	startSuspensionTime := time.Date(parkTime.Year(), parkTime.Month(), parkTime.Day(), 22, 0, 0, 0, time.UTC)
+	endSuspensionTime := time.Date(leaveTime.Year(), leaveTime.Month(), leaveTime.Day(), suspensionEndTime, 0, 0, 0, time.UTC)
 	if leaveTime.After(startSuspensionTime) {
 		totalPrice += suspensionFee
 		actualMinutes -= leaveTime.Sub(startSuspensionTime).Minutes()
+		if leaveTime.After(endSuspensionTime) {
+			actualMinutes += leaveTime.Sub(endSuspensionTime).Minutes()
+		}
 		if startSuspensionTime.Sub(parkTime).Minutes() > freeParkingTime() {
 			totalPrice += CalculateParkingPriceWithTime(actualMinutes, pricePerHour)
 		}
@@ -92,8 +106,7 @@ func CalculateParkingFee(parkTime, leaveTime time.Time) (int, error) {
 }
 
 func freeParkingTime() float64 {
-	freeTime := time.Hour * freeParkingHours
-	return freeTime.Minutes()
+	return time.Hour.Minutes() * freeParkingHours
 }
 
 func CalculateParkingPriceWithTime(actualMinutes float64, pricePerHour int) int {
